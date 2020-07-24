6a0dc9b909e999c2a4cef150fdcfd94c7908f6fe
Paul Bredenberg <paulbredenberg@gmail.com>
2020-07-24 14:22:10 -0400

    feat: provide stricter version range to auto-changelog

--- src/auto-changelog-config/index.ts
+++ src/auto-changelog-config/index.ts
@@ -1,5 +1,6 @@
 import path from 'path';
-import { CHANGELOG_INFILE } from '../index';
+import { CHANGELOG_INFILE, LATEST_VALID_TAG_COMMAND } from '../index';
+import executeShellCommand from '../utilities/execute-shell-command';
 
 const CHANGELOG_HEADER =
 `# Changelog
@@ -16,17 +17,18 @@ const AUTOCHANGELOG_TEMPLATE_PATH = `${path.resolve(__dirname)}/templates/templa
 
 // Generates auto-changlog command with options
 const autoChangelogCommand = async (): Promise<string> => {
-   const changelogPath = `${process.cwd()}/${CHANGELOG_INFILE}`;
+   const changelogPath = `${process.cwd()}/${CHANGELOG_INFILE}`,
+         getLatestValidTag = await executeShellCommand(LATEST_VALID_TAG_COMMAND, 'Getting latest valid tag');
 
    return [
       'npx',
       'auto-changelog',
-      '-p',
       '--commit-limit false',
+      '--backfill-limit false',
       `--template ${AUTOCHANGELOG_TEMPLATE_PATH}`,
       `--output ${changelogPath}`,
-      '--unreleased-only',
       '--stdout',
+      `--starting-version ${getLatestValidTag}`,
    ]
       .join(' ');
 };
