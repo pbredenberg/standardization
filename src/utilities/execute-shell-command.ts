08dbe7ee3c06e241bccadd40e046142964044cf7
Paul Bredenberg <paulbredenberg@gmail.com>
2020-08-14 11:31:06 -0400

    feat: allow silent operation when executing a shell command

--- src/utilities/execute-shell-command.ts
+++ src/utilities/execute-shell-command.ts
@@ -6,9 +6,11 @@ import { exec } from 'child_process';
  * @param command The command to run in shell
  * @param logMessage Message to print before command execution
  */
-export default (command: string, logMessage: string): Promise<string> => {
+export default (command: string, logMessage?: string): Promise<string> => {
 
-   console.log(`${logMessage}:`, command); // eslint-disable-line
+   if (logMessage) {
+      console.log(`${logMessage}:`, command); // eslint-disable-line
+   }
 
    return new Promise((resolve, reject) => {
       exec(
